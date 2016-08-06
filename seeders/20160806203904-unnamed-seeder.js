b63d6356522d81fc75135a05ff669f9627cf129a
Christie R <creindle@hawaii.edu>
2016-08-06 11:40:47 -1000

    All the seeds and migrations work now :)

---
+++ seeders/20160806203904-unnamed-seeder.js
@@ -0,0 +1,26 @@
+'use strict';
+const faker = require('faker');
+
+var pictureObject = [];
+for (var i = 0; i < 50; i++){
+  var temp = {
+    url : faker.image.imageUrl(500, 500, "abstract") + '/' + Math.floor(Math.random() * 11),
+    author : faker.name.firstName() + " " + faker.name.firstName(),
+    description : faker.lorem.sentence(),
+    createdAt : new Date(),
+    updatedAt : new Date()
+  }
+  pictureObject.push(temp);
+}
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Pictures',
+      pictureObject
+    , {})
+  },
+
+  down: function (queryInterface, Sequelize) {
+    queryInterface.bulkDelete('Pictures')
+  }
+};
